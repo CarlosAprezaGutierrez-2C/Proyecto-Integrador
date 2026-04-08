734e642ab3c73822754140dbef97c835c3d0b2f8
IXAYA <20253ds067@utez.edu.mx>
2026-04-08 00:43:13 -0600

    cambios finales

--- integradora/src/main/java/module-info.java
+++ integradora/src/main/java/module-info.java
@@ -6,13 +6,7 @@
     requires javafx.base;
     requires javafx.graphics;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
+
 
     opens com.example.integradora to javafx.fxml;
     opens com.example.integradora.controllers to javafx.fxml;
